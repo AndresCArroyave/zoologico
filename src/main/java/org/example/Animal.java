f84ebf6cb3c652abd3a47771c4b395f6e8da9b17
Andres Camilo Arroyave <andres1799@gmail.com>
2023-08-21 18:49:36 -0500

    Se ultiman detalles del examen

--- src/main/java/org/example/Animal.java
+++ src/main/java/org/example/Animal.java
@@ -2,24 +2,56 @@ package org.example;
 
 public class Animal {
 
-    String nombre;
-    String genero
-    double peso=true;
-    Animal pareja=new;
+    private String nombre;
+    private String genero;
+    private  Double peso;
 
+    private Animal pareja;
 
-    public Animal(String nombre, double peso) {
-        this.nombre = nombre;
-        this.peso = peso;
+    public Animal() {
     }
 
-    void encontrarPareja(Animal pareja){
+    public Animal(String nombre, Double peso, String genero) {
+        this.nombre = nombre;
+        this.peso = peso;
+        this.genero= genero;
+    }
+
+    public void encontrarPareja(Animal pareja){
         this.pareja=pareja;
     }
 
+    public String getNombre() {
+        return nombre;
+    }
 
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 
+    public String getGenero() {
+        return genero;
+    }
 
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public Double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(Double peso) {
+        this.peso = peso;
+    }
+
+    public Animal getPareja() {
+        return pareja;
+    }
+
+    public void setPareja(Animal pareja) {
+        this.pareja = pareja;
+    }
 
 
 }
